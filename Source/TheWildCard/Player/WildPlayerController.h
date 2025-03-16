427bd8d10f92f97d2df8ab77e47d611cd4d8c7a6
Dongju Jang <71020187+dongdong2222@users.noreply.github.com>
2025-03-16 18:14:17 +0900

    Add Unit Spawn with Card

---
+++ Source/TheWildCard/Player/WildPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "WildPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEWILDCARD_API AWildPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
